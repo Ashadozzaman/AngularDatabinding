c68ea1b12be04391910d49b7c9c88e1193ff0051
Ashadozzaman <shovoua@gmail.com>
2020-04-01 16:43:46 +0600

    complete the property binding section and component

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,OnChanges,OnDestroy{
   @Input('srv_element') element: {type:string,name:string,content:string};
-  constructor() { }
+  @Input() name:string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph:ElementRef;
+  constructor() {
+    console.log('Constructior')
+   }
 
+   ngOnChanges(changes: SimpleChanges ){
+      console.log('onChanges Call');
+      console.log('Changes');
+
+   }
   ngOnInit(): void {
-  }
+    console.log('ngInit call!');
+    console.log('Text Content:'+this.header.nativeElement.textContent);
+    console.log('Test:'+ this.paragraph.nativeElement.textContent);
+    }
 
+  ngOnDestroy(){
+    console.log('ngdestroy call!')
+  }
 }
